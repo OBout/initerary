db36827c6c26ff989fdfdfd9b02cc7966c4f7071
Oscar Bout <o.bout@diract-it.nl>
2017-12-23 18:19:15 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { EditPage } from '../pages/edit/edit';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { LoginPage } from '../directives/login/login';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDEQObnRPUgSmUO8jAunBvNxhg7Vue-Gpg",
+  authDomain: "itinerary-oscoweb.firebaseapp.com",
+  databaseURL: "https://itinerary-oscoweb.firebaseio.com",
+  projectId: "itinerary-oscoweb",
+  storageBucket: "itinerary-oscoweb.appspot.com",
+  messagingSenderId: "231837315466"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    EditPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    EditPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuth,
+    AngularFireDatabase
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {AngularFireAuthModule, AngularFireAuth} from 'angularfire2/auth';
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
+@Component({selector: 'page-home', templateUrl: 'home.html'})
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  days : FirebaseListObservable < any[] >;
+  select_day : number = -1;
 
+  constructor(public navCtrl : NavController, private db : AngularFireDatabase) {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDEQObnRPUgSmUO8jAunBvNxhg7Vue-Gpg",
+      authDomain: "itinerary-oscoweb.firebaseapp.com",
+      databaseURL: "https://itinerary-oscoweb.firebaseio.com",
+      projectId: "itinerary-oscoweb",
+      storageBucket: "itinerary-oscoweb.appspot.com",
+      messagingSenderId: "231837315466"
+    };
+
+    this.days = this
+      .db
+      .list('/Itinerary');
+    this
+      .days
+      .subscribe((response) => {
+        console.log('response', response);
+      }, (error) => {
+        console.log('error', error);
+      });
   }
+
+  selectDay(i : number) {
+    this.select_day = i;
+  }
+
+  daySelected() : number {return this.select_day;}
 
 }
